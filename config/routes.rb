c77d7b63da4e3bfc41ec739ebd9a742c64b8d0a1
ben <wbpiggot@gmail.com>
2016-06-27 23:25:40 -0700

    New project feature basic implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "projects#index"
+
+  resource :projects
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
