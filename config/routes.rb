3756668c422dcea5b4a00f51c25f30bb5185652a
wbp <wbpiggot@gmail.com>
2016-07-08 01:10:39 -0400

    Fixing routing for Admin::Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :admin do
     root 'application#index'
-    
+      
+    resources :users
     resources :projects, only: [:new, :create, :destroy]
   end
-
+  
   devise_for :users
   root "projects#index"
 
