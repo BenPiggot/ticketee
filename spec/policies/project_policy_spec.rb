a67be7c546221943f2618aea0ca2c378d0c7d81c
ben <wbpiggot@gmail.com>
2016-07-10 21:40:53 -0700

    wip

--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -11,7 +11,33 @@ RSpec.describe ProjectPolicy do
   end
 
   permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    let(:user) { FactoryGirl.create(:user) }
+    let(:project) { FactoryGirl.create(:project) } 
+
+    it "blocks anonymous users" do
+      expect(subject).not_to permit(nil, project)
+    end
+
+    it "allows viewers of the porject" do
+      assign_role!(user, :viewer, project) 
+      expect(subject).to permit(user, project)
+    end
+
+    it "allows editors of the project" do
+      assign_role!(user, :editor, project)
+      expect(subject).to permit(user, project)
+    end
+
+    it "allows administrators" do
+      admin = FactoryGirl.create(:user, :admin) 
+      expect(subject).to permit(user, project)
+    end
+
+    it "doesn't allow users assigned to other projects" do
+      other_project = FactoryGirl.create(:project)
+      assign_role!(user, :manager, other_project)
+      expect(subject).not_to permit(user, project)
+    end
   end
 
   permissions :create? do
